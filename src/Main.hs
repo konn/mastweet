8dcc1e886af89b4cd91bc3dd62e9ee5fd2029dca
Hiromi ISHII <konn.jinro@gmail.com>
2017-09-29 20:37:43 +0900

    Supported Template

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE DeriveGeneric, ExtendedDefaultRules, LambdaCase #-}
-{-# LANGUAGE OverloadedStrings, RecordWildCards              #-}
-{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# LANGUAGE DeriveGeneric, ExtendedDefaultRules, LambdaCase    #-}
+{-# LANGUAGE NamedFieldPuns, OverloadedStrings, RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-type-defaults -Wno-orphans #-}
 module Main where
 import           Control.Exception
 import           Control.Lens
@@ -13,14 +13,16 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 import           Data.Maybe
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding         as T
+import qualified Data.Text.Lazy             as LT
 import           Data.Yaml                  (decodeFileEither)
 import           GHC.Generics               (Generic)
 import           Network.WebSockets
 import           System.Environment
 import           Text.HTML.TagSoup
+import           Text.Mustache
 import           Web.Hastodon               (Status (..), accountUsername)
 import qualified Web.Twitter.Conduit        as Tw
-import           Wuss                       hiding (Config)
+import           Wuss                       hiding (Config, defaultConfig)
 
 default (String)
 
@@ -46,9 +48,15 @@ data Mastodon = Mastodon { mastInstance     :: String
 
 data Config = Config { twitter  :: Twitter
                      , mastodon :: Mastodon
+                     , template :: Template
                      }
-            deriving (Read, Show, Eq, Ord, Generic)
+            deriving ( Show, Eq, Ord, Generic)
 
+
+instance FromJSON Template where
+  parseJSON = withText "valid mustache template" $ \t ->
+    either (fail . show) return $
+    compileMustacheText "template" (LT.fromStrict t)
 
 mastOpts, cnfOpts :: Options
 mastOpts = defaultOptions { fieldLabelModifier = camelTo2 '_' . drop 4}
@@ -64,24 +72,17 @@ instance ToJSON Mastodon where
 instance FromJSON Twitter where
   parseJSON = genericParseJSON mastOpts
 
-instance ToJSON Twitter where
-  toJSON = genericToJSON mastOpts
-  toEncoding = genericToEncoding mastOpts
-
 instance FromJSON Config where
   parseJSON = genericParseJSON cnfOpts
-
-instance ToJSON Config where
-  toJSON = genericToJSON mastOpts
-  toEncoding = genericToEncoding cnfOpts
 
 main :: IO ()
 main = do
   fp <- fromMaybe "settings.yaml" . listToMaybe <$> getArgs
   decodeFileEither fp >>= \case
     Left err -> throwIO err
-    Right Config{mastodon = Mastodon{..}, twitter = Twitter{..}} ->
-      ignoreParseError $ runSecureClientWith
+    Right Config{template, mastodon = Mastodon{..}, twitter = Twitter{..}} ->
+      ignoreParseError $
+      runSecureClientWith
           mastInstance
           443
           "/api/v1/streaming?stream=user"
@@ -89,7 +90,7 @@ main = do
           [("Authorization", BS.pack $ "Bearer " ++ mastAccessToken)] $ \conn ->
           forever $ do
             mst <- getStatus <$> receiveData conn
-            forM_ mst $ \Status{..} -> do
+            forM_ mst $ \(val, Status{..}) -> do
               man <- Tw.newManager Tw.tlsManagerSettings
               let oauth = Tw.twitterOAuth { Tw.oauthConsumerKey = BS.pack twitConsumerKey
                                           , Tw.oauthConsumerSecret = BS.pack twitConsumerSecret
@@ -98,11 +99,11 @@ main = do
                                         ,("oauth_token_secret", BS.pack twitAccessTokenSecret)
                                         ]
                   toot  = fromTagText =<< filter isTagText (parseTags statusContent)
-                  tweet = T.pack $ toot ++ " [Mastodonより: " ++ statusUrl ++ "]"
                   go    = null statusMentions && accountUsername statusAccount == mastUserName
+                  tweet = renderMustache template (val & key "content" . _String .~ T.pack toot)
               when go $ discardValue $
                 Tw.call' (Tw.setCredential oauth creds Tw.def) man $
-                Tw.update tweet
+                Tw.update $ LT.toStrict tweet
 
 
 
@@ -117,5 +118,7 @@ ignoreParseError act =
         _ -> return ()
   in loop
 
-getStatus :: BS.ByteString -> Maybe Status
-getStatus obj = obj ^? key "payload" . _String . to T.encodeUtf8 >>= decode . LBS.fromStrict
+getStatus :: BS.ByteString -> Maybe (Value, Status)
+getStatus obj = do
+  src <- obj ^? key "payload" . _String . to (LBS.fromStrict . T.encodeUtf8)
+  (,) <$> decode src <*> decode src
